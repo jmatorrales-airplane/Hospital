7f092cc906bfcf7c697f9a4f78167b10e036f058
jmatorrales-airplane <jordi.matorrales@airplane.solutions>
2022-12-02 13:51:03 +0100

    añadidos medicación y pruebas realizadas al paciente al metodo ingreso

--- Paciente.cs
+++ Paciente.cs
@@ -7,8 +7,8 @@ namespace jmatorrales.hospital
         public string diagnostico { get; set; }
         public int diasDeIngreso { get; set; }
         public char pronostico { get; set; }
-        public string[] medicacion { get; set; }
-        public string[] pruebas { get; set; }
+        public List<string> medicamentos { get; set; }
+        public List<string> pruebas { get; set; }
         public bool esPaciente { get; set; }
 
         public void altaPaciente()
@@ -16,16 +16,16 @@ namespace jmatorrales.hospital
 
         }
 
-        public void ingreso(string nombre, string direccion, string dni, string diagnostico, int diasDeIngreso, char pronostico, string medicamentos, string pruebas)
+        public void ingreso(string nombre, string direccion, string dni, string diagnostico, int diasDeIngreso, char pronostico, string medicacion, string pruebas)
         {
             this.nombre = nombre;
             this.direccion = direccion;
-
             this.dni = dni;
-            
             this.diagnostico = diagnostico;
             this.diasDeIngreso = diasDeIngreso;
             this.pronostico = pronostico;
+            medicamentos.Add(medicacion);
+            this.pruebas.Add(pruebas);
             esPaciente = true;
         }
     }
